240976d2622633d6c8cad16089555c12b3111a2a
jamil1621 <mashud15-3739@diu.edu.bd>
2023-08-08 21:14:34 +0600

    Bmi Calculator

---
+++ day02.py
@@ -0,0 +1,15 @@
+#Bmi Calculator
+
+weight = input("enter your weight in kg: ")
+height = input("enter your height in m: ")
+
+
+#print(height)
+#print(weight)
+
+height = float(height)*float(height)
+
+bmi = float(weight) / float(height)
+
+print(float(bmi))
+print(int(bmi))
