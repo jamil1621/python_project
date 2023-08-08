96ee110dc214ad6f89555303a460b8a589342614
jamil1621 <mashud15-3739@diu.edu.bd>
2023-08-08 19:33:36 +0600

    Brand name generator small project

---
+++ new01.py
@@ -0,0 +1,9 @@
+#Brand name generator small project
+
+print("Welcome to this simple project. ")
+
+location = input("Where did you grew up?: \n")
+pet_name = input("what is your pet name?: \n")
+
+print("your brand name may be: " + location + pet_name)
+
